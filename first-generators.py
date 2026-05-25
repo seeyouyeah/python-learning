eef51fa910bf986f305e8f668aced4c9cefa81fc
Tony <seeyouyeah33@gmail.com>
2026-05-25 11:25:55 +0300

    new attempt

--- first-generators.py
+++ first-generators.py
@@ -1,4 +1,4 @@
-# # # задание 1 изи
+# # # # задание 1 изи
 # # # def countdown(n):
 # # #     for i in range(n, 0, -1):
 # # #         yield i
@@ -19,47 +19,35 @@
 # #     print(i)
 
 
+# # задание 3 Напиши генератор merge_sorted(*iterables), который принимает несколько уже отсортированных списков и отдаёт элементы в общем отсортированном порядке — без использования sorted() и без объединения списков в один. (решение от Клода)
+# def merge_sorted(*iterables):
+#     pointers = [0] * len(iterables)
+#     while True:
+#         current = [
+#             (iterables[k][pointers[k]], k)  # (значение, индекс списка)
+#             for k in range(len(iterables))
+#             if pointers[k] < len(iterables[k])  # список ещё не кончился
+#         ]
+        
+#         if not current:  # все списки кончились
+#             break
+        
+#         value, k = min(current)
+#         yield value
+#         pointers[k] += 1  # сдвинь указатель того списка
+        
+# for i in merge_sorted([1, 5], [3, 4], [-10, 0, 12], [2]):
+#     print(i)
 
 
-# # задание 3 хард
-def merge_sorted(*iterables):
-    pointers = [0] * len(iterables)
-    while True:
-        # текущие элементы всех незаконченных списков
-        current = [
-            (iterables[k][pointers[k]], k)  # (значение, индекс списка)
-            for k in range(len(iterables))
-            if pointers[k] < len(iterables[k])  # список ещё не кончился
-        ]
-        
-        if not current:  # все списки кончились
-            break
-        
-        value, k = min(current)  # найди минимальный
-        yield value
-        pointers[k] += 1  # сдвинь указатель того списка
-        
-for i in merge_sorted([1, 5], [3, 4], [-10, 0, 12], [2]):
+
+
+def unique(args):
+    yielded = set()
+    for i in args:
+        if i not in yielded:
+            yielded.add(i)
+            yield i
+            
+for i in unique([1, 3, 2, 1, 5, 3, 4, 2]):
     print(i)
-
-
-
-
-
-
-# iterables = [[1, 5], [3, 4], [12, -10, 0], [2]]
-
-# pointers = [0] * len(iterables)
-# print(pointers)
-# # current = [i for i in range(len(iterables)) if pointers[i] < len(iterables[i])]
-# current = [
-#         (iterables[k][pointers[k]], k)  # (значение, индекс списка)
-#         for k in range(len(iterables))
-#         if pointers[k] < len(iterables[k])  # список ещё не кончился
-#     ]
-
-
-# print(current)
-# value, k = min(current)  # найди минимальный
-
-# print(value)
